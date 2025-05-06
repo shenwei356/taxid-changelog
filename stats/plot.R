e8c3e19fae9e38de9b405ec3bdaaa1509aabefc1
Wei Shen <shenwei356@gmail.com>
2025-05-06 11:20:29 +0800

    update to 2025.05.01

--- stats/plot.R
+++ stats/plot.R
@@ -10,15 +10,15 @@ theme1 <- theme(
     panel.background = element_blank(),
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
-    
+
     legend.position = "top",
     legend.spacing.x = unit(0.4, "cm"),
     legend.text = element_text(size = 11),
     legend.margin = margin(0.1, 0.1, 0.1, 0.1, unit = "cm"),
-    
-    plot.title = element_text(size = 15),        
+
+    plot.title = element_text(size = 15),
     text = element_text(size = 10),
-    
+
     axis.text.x = element_text(size = 10),
     axis.text.y = element_text(size = 10),
     axis.ticks.y = element_line(size = 0.6),
@@ -38,10 +38,10 @@ p <- ggplot(df, aes(x = date, y = count, color = types, shape = types)) +
   geom_point(aes(shape = types), size = 1.8) +
   scale_color_colorblind() +
   xlab(NULL) +
-  ylab("#TaxIds") + 
+  ylab("#TaxIds") +
   theme1 +
   labs(title = "NCBI Taxonomy TaxID Changes",
-    subtitle = "2014-08-01 ~ 2024-11-01",
+    subtitle = "2014-08-01 ~ 2025-05-01",
     caption = "https://github.com/shenwei356/taxid-changelog"
   )
 
